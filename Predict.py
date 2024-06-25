70503faf33bcd89e44a373754c8e8c038de27746
Krushnansh22 <krushnansh22@gmail.com>
2024-06-25 22:27:21 +0530

    .

--- Predict.py
+++ Predict.py
@@ -3,6 +3,9 @@ import pickle
 import time
 import AddToExcel
 
+
+# Flag to check current status
+
 def load_model(model_path):
     # Load the trained face recognizer model
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -18,6 +21,7 @@ def load_label_dict(label_dict_path):
 
 
 def predict_faces(face_recognizer, label_dict, threshold=80):
+    global locked_status, current_status
     cap = cv2.VideoCapture(0)
     detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     reverse_label_dict = {v: k for k, v in label_dict.items()}
@@ -32,25 +36,33 @@ def predict_faces(face_recognizer, label_dict, threshold=80):
         grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = detector.detectMultiScale(grey, 1.1, 4)
 
-        cv2.putText(img, "U : Unlock", (20, 450), 1, 2, (0, 0, 255), 2)
+        cv2.putText(img, "L : Lock/Unlock", (20, 450), 1, 2, (0, 0, 255), 2)
 
         for (x, y, w, h) in faces:
             face = grey[y:y + h, x:x + w]
             label_id, confidence = face_recognizer.predict(face)
+
+            cv2.putText(img, f"{current_status}", (430, 50), 1, 2, (100, 100, 0), 3)
 
             if confidence > threshold:
                 label = "unknown"
             else:
                 label = reverse_label_dict[label_id]
 
-            if cv2.waitKey(1) == ord('u'):
-                if label == "unknown":
-                    display_text = "Unauthorised Personnel"
-                    # Record Unauthorised access attempts
-                    AddToExcel.unauthorized_unlock('unauthaurised.xlsx',img)
-                else:
-                    display_text = "Unlocked"
-                text_start_time = time.time()
+            if locked_status:
+                if cv2.waitKey(1) == ord('l'):
+                    if label == "unknown":
+                        display_text = "Unauthorised Personnel"
+                        text_start_time = time.time()
+                        # Record Unauthorised access attempts
+                        AddToExcel.unauthorized_unlock('unauthaurised.xlsx', img)
+                    else:
+                        locked_status = not locked_status
+                        current_status = "Unlocked"
+            elif not locked_status:
+                if cv2.waitKey(1) == ord('l'):
+                    current_status = "Locked"
+                    locked_status = not locked_status
 
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
@@ -58,7 +70,6 @@ def predict_faces(face_recognizer, label_dict, threshold=80):
         # Check if text should be displayed
         if display_text and (time.time() - text_start_time < display_duration):
             cv2.putText(img, display_text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-
 
         cv2.imshow('Face Recognition', img)
 
@@ -70,6 +81,8 @@ def predict_faces(face_recognizer, label_dict, threshold=80):
 
 
 if __name__ == "__main__":
+    locked_status = True
+    current_status = "Locked"
     face_recognizer = load_model('face_recognizer_model.xml')
     label_dict = load_label_dict('label_dict.pkl')
 
